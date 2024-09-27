23cc79d8d93941066a24ef4ee91757d01764b790
Aman Kumar <kumaraman140795@gmail.com>
2024-09-27 09:00:32 +0530

    Create runner and transform setup

---
+++ src/runner.js
@@ -0,0 +1,27 @@
+const { run } = require('jscodeshift/src/Runner');
+const path = require('path');
+
+// path for transform file, files to transform, and jscodeshift runner optionsr
+const transformPath = path.resolve('src/transform.js');
+const paths = ['test_files'];
+
+console.log(transformPath);
+console.log(paths);
+
+const options = {
+    verbose: 2,
+    dry: true,
+    print: true,
+    runInBand: false,
+    silent: false,
+    failOnError: true,
+    stdin: false,
+    ignorePattern: [],
+    ignoreConfig: [],
+    gitignore: '.gitignore',
+    // babel: true,
+    // parser: 'babel',
+    // extensions: 'cjs, mjs, js, jsx, ts, tsx',
+};
+
+run(transformPath, paths, options).then(console.log).catch(console.error);
---
+++ src/transform.js
@@ -0,0 +1,6 @@
+const transform = (fileInfo, api, options) => {
+    console.log(fileInfo);
+    return fileInfo.source;
+}
+
+module.exports = transform;
