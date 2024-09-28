c1b2c514973a56e3e130b7d542c6e5d264f8c336
Aman Kumar <kumaraman140795@gmail.com>
2024-09-29 01:27:12 +0530

    Create utility to create function call AST subtree

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 const { RENDER_ATTRS, RENDER_ATTRS_BY_TAG } = require("../constants/index");
 
+// returns if an attribute's value is rendered on screen
 const isAttributeRenderable = (tag, attribute) => {
     if(RENDER_ATTRS[attribute] === true || (
         RENDER_ATTRS_BY_TAG[tag] && RENDER_ATTRS_BY_TAG[tag][attribute] === true
@@ -11,6 +12,15 @@ const isAttributeRenderable = (tag, attribute) => {
     }
 }
 
+// returns small subtree representing a call to useTransition function
+const createUseTransitionCall = (jscodeshift, label) => {
+    return jscodeshift.callExpression(
+        jscodeshift.identifier('useTransition'),
+        [jscodeshift.literal(label)]
+    );
+}
+
 module.exports = {
-    isAttributeRenderable
+    isAttributeRenderable,
+    createUseTransitionCall
 }
